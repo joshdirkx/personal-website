e2e98edd4e1508f73c4bb864992c34f24e617c59
Josh Dirkx <joshdirkx@gmail.com>
2023-08-17 08:30:30 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content p-4 bg-white bg-opacity-80 rounded-lg max-w-lg mx-auto mt-10 overflow-y-auto h-4/5">
+      <h1 className="text-2xl font-bold mb-4">Your Name</h1>
+      <p className="mb-4">Short bio or introduction...</p>
+
+      <h2 className="text-xl font-semibold mb-2">Education</h2>
+      {/* Education details here */}
+
+      <h2 className="text-xl font-semibold mb-2">Experience</h2>
+      {/* Work experience details here */}
     </div>
   );
 }
