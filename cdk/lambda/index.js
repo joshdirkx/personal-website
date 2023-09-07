7e58a26ec109a7eddc9812a45da8711550068a8e
Josh Dirkx <joshdirkx@gmail.com>
2023-09-07 14:18:19 -0700

    stuff

--- cdk/lambda/index.js
+++
@@ -1,8 +0,0 @@
-export const handler = async (event) => {
-  console.log(event)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
-};
