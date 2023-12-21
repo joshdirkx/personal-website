f05665532c73b7c94b39c38a1cfe0135df52f927
Josh Dirkx <joshdirkx@gmail.com>
2023-12-21 09:57:56 -0800

    styling

--- frontend/src/components/status/StatusLine.tsx
+++ frontend/src/components/status/StatusLine.tsx
@@ -5,19 +5,17 @@ interface StatusLineProps {
     activePane?: boolean;
 }
 
-function StatusLine({ content, activePane }: StatusLineProps) {
-    return(
+function StatusLine({content, activePane}: StatusLineProps) {
+    return (
         <div className="bottom-4 left-0 right-0 bg-solarized-base02 text-solarized-base1 py-1 px-4 text-left">
-        <div className="flex">
-  <div className="pr-2">
-            {activePane && (
-      <div className="pl-2 pr-2 bg-solarized-blue text-solarized-base03">Main</div>
-                )}
-  </div>
-  <div>{content}</div>
+            <div className="flex">
+                <div className="pr-2">
+                    {activePane && (<div className="pl-2 pr-2 bg-solarized-blue text-solarized-base03">Main</div>)}
+                </div>
+                <div>{content}</div>
+            </div>
         </div>
-</div>
     );
-};
+}
 
 export default StatusLine;
