2a8ed6bb035ffab78c1a84e2da8bd514e5782900
Josh Dirkx <joshdirkx@gmail.com>
2024-02-17 00:01:12 -0800

    add divider

--- frontend/src/components/Divider.tsx
+++ frontend/src/components/Divider.tsx
@@ -1,12 +1,10 @@
 export default function Divider() {
   return (
-    <div className="relative">
+    <div className="relative m-4">
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
-        <div className="w-full border-t border-gray-300" />
+        <div className="w-full border-t border-solarized-base01" />
       </div>
-      <div className="relative flex justify-center">
-        <span className="bg-transparent px-3 text-base font-semibold leading-6 text-gray-900">Articles</span>
-      </div>
+      <div className="relative flex justify-center pt-10" />
     </div>
-  )
+  );
 }
